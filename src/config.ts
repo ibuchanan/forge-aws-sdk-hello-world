ecb1483c7c3f15e7677034c89802a0d23492efca
Ian Buchanan <ibuchanan@atlassian.com>
2023-11-18 17:17:36 -0500

    fix: call aws-sdk, set aws egress permissions, format with prettier

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 /*
 https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
@@ -7,9 +7,9 @@ export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 export AWS_DEFAULT_REGION=us-west-2
 */
 const envSchema = z.object({
-    AWS_DEFAULT_REGION: z.string().min(1),
-    AWS_ACCESS_KEY_ID: z.string().min(1),
-    AWS_SECRET_ACCESS_KEY: z.string().min(1),
+  AWS_DEFAULT_REGION: z.string().min(1),
+  AWS_ACCESS_KEY_ID: z.string().min(1),
+  AWS_SECRET_ACCESS_KEY: z.string().min(1),
 });
 
 /*
@@ -21,9 +21,9 @@ const config = envSchema.parse({
 */
 
 const config = envSchema.parse({
-    AWS_DEFAULT_REGION: "us-west-2",
-    AWS_ACCESS_KEY_ID: "AKIAIOSFODNN7EXAMPLE",
-    AWS_SECRET_ACCESS_KEY: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+  AWS_DEFAULT_REGION: "us-west-2",
+  AWS_ACCESS_KEY_ID: "AKIAIOSFODNN7EXAMPLE",
+  AWS_SECRET_ACCESS_KEY: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
 });
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,36 @@
+import s3lifecycle from "./aws";
+
 export function listener(req: any) {
   try {
     const body = JSON.parse(req.body);
     console.log(body);
-    return s3lifecycle(body);
+    s3lifecycle(req);
+    return {
+      body: '{"hello": "world"}',
+      headers: { "Content-Type": ["application/json"] },
+      statusCode: 200,
+      statusText: "OK",
+    };
   } catch (error) {
     if (error instanceof SyntaxError) {
       const invalidJson = {
         type: "about:blank",
         title: "Invalid request",
         detail: "The request must be valid JSON",
-      }
+      };
       return {
         body: JSON.stringify(invalidJson),
         headers: { "Content-Type": ["application/problem+json"] },
         statusCode: 400,
         statusText: "Bad Request",
-      }
+      };
     } else {
       return {
         body: error + "\n",
         headers: { "Content-Type": ["application/json"] },
         statusCode: 400,
         statusText: "Bad Request",
-      }  
+      };
     }
   }
-};
+}
